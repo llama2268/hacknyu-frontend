55f0e28b703b2658c79780b364a304c23f40a198
llama2268 <tyleryang5360@gmail.com>
2025-02-08 21:14:12 -0500

    new pages

---
+++ app/interface/page.tsx
@@ -0,0 +1,10 @@
+import { title } from "@/components/primitives";
+
+export default function DocsPage() {
+  return (
+    <div>
+      <h1 className={title()}>Recipe, Saved, My History </h1>
+      <h2>generate button that takes you to a form to generate recipes</h2>
+    </div>
+  );
+}
